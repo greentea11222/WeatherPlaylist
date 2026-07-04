d8c1091d687b63772545657de6b2183961ffc09a
greentea11222 <oregreen1122@gmail.com>
2026-07-04 22:38:36 +0900

    WeatherControllerの追加

---
+++ src/main/java/com/example/demo/controller/WeatherController.java
@@ -0,0 +1,56 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.Playlist;
+import com.example.demo.dto.WeatherDetail;
+import com.example.demo.dto.WeatherResponse;
+import com.example.demo.service.WeatherService;
+
+@RestController
+@RequestMapping("/api")
+public class WeatherController {
+	
+	//WeatherServiceのインスタンスを自動的に紐づけてくれる
+	@Autowired
+	private WeatherService weatherService;
+	
+	@GetMapping("/recommend")
+	public List<Playlist> getRecommendPlaylists(@RequestParam("city") String cityName){
+		
+		//都市名を使って、天気サービスから全体のレスポンスを取得する
+		WeatherResponse response = weatherService.getWeather(cityName);
+		
+		//レスポンスから、時間ごとの天気リストを取得
+		List<WeatherDetail> weatherList = response.getWeatherList();
+		
+		//リストの先頭のデータからstatusを取得
+		String currentStatus = weatherList.get(0).getStatus();
+		
+		//全てのプレイリスト一覧を作成
+		List<Playlist> allPlaylists = new ArrayList<Playlist>();
+		
+		//ダミーのプレイリストを作成して一覧に格納
+		Playlist list1 = new Playlist();
+		list1.setStatus("SUNNY");
+		Playlist list2 = new Playlist();
+		list2.setStatus("RAINY");
+		allPlaylists.add(list1);
+		allPlaylists.add(list2);
+		
+		//全てのプレイリストから、天気が一致するものを絞り込む
+		List<Playlist> matchedPlaylists = allPlaylists.stream()
+				.filter(playlist -> playlist.getStatus().equals(currentStatus))
+				.toList();
+		
+		//絞り込んだプレイリストを返却
+		return matchedPlaylists;
+	}
+}
