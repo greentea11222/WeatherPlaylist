253113ba36395806a6b44f9e8e6143099d934b49
greentea11222 <oregreen1122@gmail.com>
2026-07-06 22:00:32 +0900

    WeatherServiceのメソッドを修正（APIと繋げた）

--- src/main/java/com/example/demo/dto/WeatherResponse.java
+++ src/main/java/com/example/demo/dto/WeatherResponse.java
@@ -1,12 +1,10 @@
 package com.example.demo.dto;
 
-import java.util.List;
-
 import lombok.Data;
 
 @Data
 public class WeatherResponse {
 	private String cityName;
 	
-	private List<WeatherDetail> weatherList;
+	private CUrrentWeather current;
 }
