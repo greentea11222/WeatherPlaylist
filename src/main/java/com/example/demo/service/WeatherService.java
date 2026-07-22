750817b09a62389a3e1bdd129773b1ad9845c03e
greentea11222 <oregreen1122@gmail.com>
2026-07-22 23:11:13 +0900

    WeatherServiceに各都市の緯度と経度をswitch文で設定

--- src/main/java/com/example/demo/service/WeatherService.java
+++ src/main/java/com/example/demo/service/WeatherService.java
@@ -8,6 +8,29 @@ import com.example.demo.dto.WeatherResponse;
 @Service
 public class WeatherService {
 	public WeatherResponse getWeather(String cityName) {
+		// 都市名(cityName)に応じて、緯度(lat)と経度(lon)を切り替える
+		// 大文字、小文字の違いを無視するために、toLowerCase()を使用する
+		double latitude;
+		double longitude;
+		
+		switch (cityName.toLowerCase()) {
+		case "osaka" -> {
+			latitude = 34.6937;
+			longitude = 135.5023;
+		}
+		case "sapporo" -> {
+			latitude = 43.0618;
+			longitude = 141.3545;
+		}
+		case "okinawa" -> {
+			latitude = 26.2124;
+			longitude = 127.6809;
+		}
+		default -> { //デフォルトは東京
+			latitude = 35.6785;
+			longitude = 139.6823;
+		}
+		}
 		
 		//外部のお天気API（Open-Meteo)のURL
 		String url = "https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&current=temperature_2m,weather_code&timezone=Asia/Tokyo";
