a370e1a14772c32a61efa7339b121118f144e893
greentea11222 <oregreen1122@gmail.com>
2026-09-03 20:27:20 +0900

    Geocoding APIを利用

--- src/main/java/com/example/demo/service/WeatherService.java
+++ src/main/java/com/example/demo/service/WeatherService.java
@@ -5,36 +5,34 @@ import java.util.Locale;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.example.demo.dto.GeocodingResponse;
 import com.example.demo.dto.WeatherResponse;
 
 @Service
 public class WeatherService {
 	
-	private final RestTemplate restTemplate = new RestTemplate();
-	
+	//Geocoding APIを使用して、都市名から天気を自動取得する
 	public WeatherResponse getWeather(String cityName) {
 		
-		String cityKey = (cityName != null ) ? cityName.trim().toUpperCase() : "TOKYO";
+		String query = (cityName != null && !cityName.trim().isEmpty()) ? cityName.trim() : "Tokyo";
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		//Open-Meteo Geocoding APIのURL
+		String geoUrl = String.format("https://geocoding-api.open-meteo.com/v1/search?name=%s&count=1&language=en&format=json", query);
+		
+		
+		
+		try {
+			GeocodingResponse geoResponse = restTemplate.getForObject(geoUrl, GeocodingResponse.class);
+		} catch (Exception e) {
+			System.out.println("Geocoding API呼び出しエラー" + e.getMessage());
+		}
 		
 		// 都市名(cityName)に応じて、緯度(lat)と経度(lon)を切り替える
 		// 大文字、小文字の違いを無視するために、toLowerCase()を使用する
-		double lat;
-		double lon;
-		
-		switch (cityKey) {
-			case "OSAKA" -> { lat = 34.6937; lon = 135.5023; }
-			case "SAPPORO" -> { lat = 43.0618; lon = 141.3545; }
-			case "OKINAWA", "NAHA" -> { lat = 26.2124; lon = 127.6809; }
-			case "KYOTO" -> { lat = 35.0116; lon = 135.7681; }
-			case "FUKUOKA" -> { lat = 33.5902; lon = 130.4017; }
-			case "LONDON" -> { lat = 51.5074; lon = -0.1278; }
-			case "NEW YORK", "NEWYORK" -> { lat = 40.7128; lon = -74.0060; }
-			case "PARIS" -> { lat = 48.8566; lon = 2.3522; }
-			default -> { //デフォルトは東京
-				lat = 35.6785;
-				lon = 139.6823;
-			}
-		}
+		double lat = 1;
+		double lon = 1;
 	
 		return getWeatherByCoordinates(lat, lon);
 	}
